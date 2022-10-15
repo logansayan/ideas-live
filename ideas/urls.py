b0df76a0a67a03ed0ee446e63920a6af3ddef7e3
logansayan <dassayan375@gmail.com>
2022-10-15 11:00:34 +0530

    add password reset functionality

--- ideas/urls.py
+++ ideas/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,7 +23,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("notes.urls")),
-    path("", include("users.urls"))
+    path("", include("users.urls")),
+    path("reset_password", auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
+    path("reset_password_sent", auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="reset_confirm.html"), name="password_reset_confirm"),
+    path("reset_password_complete", auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), name="password_reset_complete")
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
